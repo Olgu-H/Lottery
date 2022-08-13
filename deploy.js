85e66f0eb0ae5baf2799b67f258e5b7d6f1b4c7e
Olgu HOROZ <olguma18@gmail.com>
2022-08-13 16:32:04 +0300

    first commit

---
+++ deploy.js
@@ -0,0 +1,24 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'holiday mirror talk bundle acquire birth nerve obvious blame sorry boat foster',
+
+  'https://rinkeby.infura.io/v3/62fa5aec71284abd800c4763cf4f7d3c'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
